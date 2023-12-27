d6ea00006ee18819ddecec2e0cdf0b8c7a7eea9f
chao_wang <chao_wang@gmail.com>
2023-12-27 16:44:57 +0800

    feat(layout): tsconfig 、layout menu basic

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,16 @@
+import Layout from '~/components/Layout/index.vue'
+
 export const routes = [
-  { path: '/', component: () => import('../pages/Index.vue') },
-  { path: '/about', component: () => import('../pages/About.vue') },
-  { path: '/home', component: () => import('../pages/Home.vue') },
+  { path: '/', name: 'Dashboard', icon: 'Menu', component: () => import('~/pages/Index.vue') },
+  // { path: '/:pathMatch(.*)*', hidden: true, name: 'NotFound', component: () => import('~/pages/NotFound.vue') },
+  {
+    path: '/system',
+    name: '系统管理',
+    component: Layout,
+    children: [
+      { path: '/user', name: '用户管理', component: () => import('~/pages/system/User.vue') },
+      { path: '/menu', name: '菜单管理', component: () => import('~/pages/system/Menu.vue') },
+      { path: '/rule', name: '角色管理', component: () => import('~/pages/system/Rule.vue') },
+    ],
+  },
 ]
